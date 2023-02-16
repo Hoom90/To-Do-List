ae136b753e9045100cdd4053e38216abfb87c62a
Hoom90 <mohammad.mahdavi0015@gmail.com>
2023-02-16 12:43:08 +0330

    modified:   Style.css 	modified:   main.js commit

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const createBtn = document.getElementById("createBtn");
 const saveBtn = document.getElementById("saveBtn");
 const closeBtn = document.getElementById("closeBtn");
 const searchBox = document.getElementById("searchBox");
-const listItem = document.getElementById("listItem");
 
 const tasks = [];
 initiate();
@@ -11,29 +10,29 @@ createBtn.addEventListener("click", createForm);
 closeBtn.addEventListener("click", closeForm);
 saveBtn.addEventListener("click", saveForm);
 searchBox.addEventListener("keyup",search);
-listItem.addEventListener("click",itemClick);
 
+//This Method is The First Function To Read To Fill 'tasks[]' 
 function initiate() {
   if (tasks.length == 0) {
     for (var i = 0; i < getLocalStorageLength(); i++) {
       tasks.push(readFromLocalStorage(i))
     }
-    initiateList();
+    initiateList(tasks);
     return;
   }
 }
-
+//This Method is Called When Create Button is Clicked
 function createForm(e) {
   var container = document.getElementById("modal");
   if(container.style.display == "block") container.style.display = "none"; 
   else container.style.display = "block"
 }
-
+//This Method is Called When Close Button is Clicked
 function closeForm(e) {
   var container = document.getElementById("modal");
   container.style.display = "none";
 }
-
+//This Method is Called When Save Button is Clicked
 function saveForm(e) {
   var activityNameData = document.getElementById("activity").value;
   if (activityNameData === "") return;
@@ -44,45 +43,44 @@ function saveForm(e) {
   }
   closeForm()
 }
-
+//This Method Saves To Storage
 function saveToLocalStorage(tasks, counter) {
   localStorage.setItem("activity" + counter, JSON.stringify(tasks[counter]));
   initiate();
 }
-
+//This Method Reads Data
 function readFromLocalStorage(counter) {
   if (getLocalStorageLength == 0) return;
   else return JSON.parse(localStorage.getItem("activity" + counter));
 }
-
+//This Method Reads Storage Length Data
 function getLocalStorageLength() {
   return localStorage.length;
 }
-
+//This Method Updates The List Of Items In To-Do Lists
 function updateList(index) {
   var listContainer = document.querySelector("ul")
-  var li = document.createElement("li")
-  var a = document.createElement("a")
-  a.textContent = tasks[index]
-  a.setAttribute('id','listItem')
-  li.appendChild(a)
-  listContainer.appendChild(li)
+  createItem(listContainer,index)
 }
 
 function initiateList(){
   var listContainer = document.querySelector("ul");
   if (!localStorage.length == 0) {
     for (var i = 0; i < tasks.length; i++) {
-      var li = document.createElement("li");
-      var a = document.createElement("a");
-      a.textContent = tasks[i];
-      a.href = "#"
-      li.appendChild(a);
-      listContainer.appendChild(li);
+      createItem(listContainer,i)
     }
   }
 }
 
+function createItem(listContainer,index){
+  var li = document.createElement("li")
+  var a = document.createElement("a")
+  a.textContent = tasks[index]
+  a.setAttribute('onclick','itemClick(this)')
+  li.appendChild(a)
+  listContainer.appendChild(li)
+}
+//This Method is Called When Search-Box is Clicked
 function search() {
   // Declare variables
   var filter, ul, li, a, i, txtValue;
@@ -100,4 +98,9 @@ function search() {
       li[i].style.display = "none";
     }
   }
+}
+
+function itemClick(listItem){
+  if(listItem.className == "clicked") listItem.className =""
+  else listItem.className = "clicked"
 }
